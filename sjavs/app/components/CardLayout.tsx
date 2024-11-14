30b9b5d5b319a64fda3b60a6dcbb2e632856f43e
John Askham <2023.201@student.setur.fo>
2024-11-14 21:24:55 +0000

    hello

--- sjavs/app/components/CardLayout.tsx
+++ sjavs/app/components/CardLayout.tsx
@@ -5,7 +5,7 @@ import Card from './Cards';
 import styles from './CardLayout.module.css';
 
 const suits = ['spades', 'hearts', 'diamonds', 'spades'];
-const ranks = ['ace', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+const ranks = ['ace', '7', '8', '9', '10', 'jack', 'queen', 'king'];
 const imageName = `${ranks}_of_${suits}.png`;
 
 const CardLayout: React.FC = () => {
