ab1c3a194572970870a7b35020f990a62b8af1c6
Hanus Sorensen <2023.187@student.setur.fo>
2024-11-18 10:23:30 +0000

    Broytt main síduna

--- sjavs/app/page.jsx
+++ sjavs/app/page.jsx
@@ -3,16 +3,15 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <div className="main-menu">
+      <div className="log-in">
         <h2>Main Menu</h2>
       </div>
 
-      <div className="menu-options">
+      <div className="login-options">
       <p>Select an option:</p>
         <ul>
-          <li><Link href = '/joinlobby'>Join Lobby</Link></li>
-          <li><Link href = '/createlobby'>Create Lobby</Link></li>
-          <li><Link href = '/tournament'>Tournament</Link></li>
+          <li><Link href = '/signin'>Sign In</Link></li>
+          <li><Link href = '/signup'>Sign Up</Link></li>
         </ul>
       </div>
 
