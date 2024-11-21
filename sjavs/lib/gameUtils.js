11450f4108268b0fe62a656e214d2dfdeee60462
magnushansen <magnushansen124@gmail.com>
2024-11-21 17:12:07 +0000

    capital letter of cards

--- sjavs/lib/gameUtils.js
+++ sjavs/lib/gameUtils.js
@@ -1,6 +1,6 @@
 function shuffleDeck() {
     const suits = ['SPADES', 'HEARTS', 'DIAMONDS', 'CLUBS'];
-    const values = ['7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+    const values = ['7', '8', '9', '10', 'JACK', 'QUEEN', 'KING', 'ACE'];
     const deck = [];
 
     for (const suit of suits) {
