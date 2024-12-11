d03c6751ea5f1c97cd9f88c7b3e6799b26772836
John Askham <2023.201@student.setur.fo>
2024-12-11 15:36:38 +0000

    hello

--- sjavs/next.config.mjs
+++ sjavs/next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/',
-          permanent: true, // Set to `true` if this is a permanent redirect (301)
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
